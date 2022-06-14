be6cddbac614128586ba0d4a6c9c0d342db79308
DevasNAI <A01245418@itesm.mx>
2022-06-14 13:13:28 -0500

    Agrego modificacion de nombre

--- LectorCanciones.py
+++ LectorCanciones.py
@@ -1,15 +1,5 @@
 import vlc #importing vlc module
-import serial #importing serial module
-#Configure the serial port
-ser = serial.Serial(
-            '/dev/ttyACM0',  #port detected for Arduino board
-            9600,  #baud rate for serial communication
-            timeout=0,
-            bytesize=serial.EIGHTBITS,
-            parity=serial.PARITY_NONE,
-            stopbits=serial.STOPBITS_ONE
-            )
-
+import serial #importing serial module            )
 #create the instance for the vlc media player
 instance = vlc.Instance('--aout=alsa')
 #creating a new media player 
@@ -22,13 +12,6 @@ media = instance.media_new("/home/RayoMcqueen37/Music/TE2003B-Reto-Equipo-2/poke
 player.set_media(media) 
 #play the audio in the audio player
 player.play()
-while True:
-    #Wait until there is data waiting in the serial buffer
-    if (ser.in_waiting > 0):
-    #read the 8-bit data and decode to ascii
-    line = ser.read().decode("Ascii")
-    print(line)
-    if line == 'a': 
-        player.pause()
-    else:
-        pass
+for i in range(2000):
+	print("waiting")
+player.pause()
